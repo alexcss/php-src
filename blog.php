ecf85fed2e8fea930ed7615e3666cacf740a6ea5
alexcss <kalyuzhnyi@gmail.com>
2020-11-13 18:21:55 +0200

    init

---
+++ blog.php
@@ -0,0 +1,13 @@
+<?php
+
+$posts = [
+
+  ['id' => 1, 'title' => 'PHP', 'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 'published' => TRUE ],
+
+  ['id' => 2, 'title' => 'JavaScript', 'content' => 'Cum, sapiente, adipisci.', 'published' => FALSE ],
+
+  ['id' => 3, 'title' => 'HTML', 'content' => 'Beatae ipsum consectetur, quod at suscipit aperiam consequuntur harum, rerum,', 'published' => FALSE ],
+
+  ['id' => 4, 'title' => 'CSS', 'content' => 'porro quasi explicabo doloremque rem saepe aut odio modi.', 'published' => TRUE ],
+
+];
